cec607ae406db6115401f423e0064554515e178a
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-04 11:00:19 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        executed();
+    }
+    public static void executed(){
+        System.out.println("I just got executed! wow");
+    }
+}
